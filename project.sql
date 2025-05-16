045ac59b697d2edf01ab8f6b77218d1221218c8e
Lê Gia Nguyên <117912492+legianguyenit@users.noreply.github.com>
2025-05-17 06:16:40 +0700

    update database

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 14, 2025 at 08:06 AM
+-- Generation Time: May 16, 2025 at 11:16 PM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.16
 
@@ -29,18 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` int NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `categories_code` varchar(50) NOT NULL,
+  `categories_name` varchar(100) NOT NULL,
+  `categories_description` text,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Áo thun', 'áo thun mát lạnh', '2025-04-29 05:40:28', '2025-04-29 05:40:28');
+INSERT INTO `categories` (`id`, `categories_code`, `categories_name`, `categories_description`, `created_at`, `updated_at`) VALUES
+(1, 'aothunnam', 'Áo thun', 'Áo nam cotton 100%', '2025-05-16 15:28:00', '2025-05-16 15:33:56'),
+(3, 'quannam1', 'Quần nam', 'quần nam đẹp', '2025-05-16 15:55:36', NULL),
+(4, 'quannam2025', 'Quần nam đen', 'quần nam đẹp', '2025-05-16 16:10:57', '2025-05-16 16:11:27');
 
 -- --------------------------------------------------------
 
@@ -50,12 +53,14 @@ INSERT INTO `categories` (`id`, `name`, `description`, `created_at`, `updated_at
 
 CREATE TABLE `products` (
   `id` int NOT NULL,
-  `category_id` int NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text,
-  `price` decimal(10,2) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `product_code` varchar(50) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_image` varchar(255) DEFAULT NULL,
+  `product_price` decimal(10,2) NOT NULL,
+  `product_description` text,
+  `categories_code` varchar(50) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -79,7 +84,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullname`, `email`, `avatar`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Lê Gia Nguyên ', 'legianguyen288@gmail.com', 'user.png', '$2y$10$.w88ExFZniG7Ltye5AthR.jy8GTCpFc4SPLVLbNwT.KHwEI4R1bD6', '2025-04-21 08:45:45', '2025-04-22 14:14:25');
+(1, 'Lê Gia Nguyên ok', 'legianguyen28855@gmail.com', 'user.png', '$2y$10$.w88ExFZniG7Ltye5AthR.jy8GTCpFc4SPLVLbNwT.KHwEI4R1bD6', '2025-04-21 08:45:45', '2025-05-14 11:01:24'),
+(10, 'hahaaâ', 'a@gmail.com', '1747211052_Asia_Commercial_Bank_logo.svg.png', '$2y$10$Qfuyxz1wtU3mpVddcFbXXe06gQWRbJJ9/90LCvKrMxMPI62Zt6Z92', '2025-05-14 08:24:12', '2025-05-14 08:36:02'),
+(16, 'NN', 'n@gmail.com', '1747387407_1747211052_Asia_Commercial_Bank_logo.svg.png', '$2y$10$3UZBWoTXFBnxY4/fd/LfweqdnEHaeEjvicPp8lepe2shuXF9ifd.q', '2025-05-16 09:23:27', NULL);
 
 --
 -- Indexes for dumped tables
@@ -89,14 +96,15 @@ INSERT INTO `users` (`id`, `fullname`, `email`, `avatar`, `password`, `created_a
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categories_code` (`categories_code`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `fk_categories_code` (`categories_code`);
 
 --
 -- Indexes for table `users`
@@ -113,7 +121,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -125,7 +133,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
@@ -135,7 +143,7 @@ ALTER TABLE `users`
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_categories_code` FOREIGN KEY (`categories_code`) REFERENCES `categories` (`categories_code`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
